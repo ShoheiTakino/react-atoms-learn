5d35488016d70f1a874d242efdf6f97da313155a
ShoheiTakino <104742356+ShoheiTakino@users.noreply.github.com>
2022-07-07 20:47:45 +0900

    organisms作成

---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img alt="プロフィール" />
+      <p>名前</p>
+      <dl>
+        <dt>メール</dt>
+        <dd>11111@aa.com</dd>
+        <dt>TEL</dt>
+        <dd>090-0000-0000</dd>
+        <dt>会社名</dt>
+        <dd>shohiiiiiii株式会社</dd>
+        <dt>WEB</dt>
+        <dd>htttttt.com</dd>
+      </dl>
+    </div>
+  );
+};
