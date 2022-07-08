dfa1008736a69b737f9fd1cc6ba4cb2944037a26
ShoheiTakino <104742356+ShoheiTakino@users.noreply.github.com>
2022-07-08 10:41:49 +0900

    Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
-import { HeaderOnly } from "./components/templates/HeaderOnly";
+import { Router } from "./router/Router";
 import "./styles.css";
 
 export default function App(props) {
@@ -18,15 +12,5 @@ export default function App(props) {
     },
     website: "https://google.com"
   };
-  return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>検索</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
