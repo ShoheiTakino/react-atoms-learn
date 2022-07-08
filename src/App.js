28fc1abcfc69dff2d98afebc6c47c9cf95a1c5bc
ShoheiTakino <104742356+ShoheiTakino@users.noreply.github.com>
2022-07-08 11:31:18 +0900

    pages作成

--- src/App.js
+++ src/App.js
@@ -2,15 +2,5 @@ import { Router } from "./router/Router";
 import "./styles.css";
 
 export default function App(props) {
-  const user = {
-    name: "shohiiiiiii",
-    image: "https://source.unsplash.com/WX4i1Jq_o0Y",
-    email: "1234@gmai;.com",
-    phone: "090-0909-0909",
-    company: {
-      name: "テスト株式会社"
-    },
-    website: "https://google.com"
-  };
   return <Router />;
 }
--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,14 +1,44 @@
 import styled from "styled-components";
 import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `shohi${val}`,
+    image: "https://source.unsplash.com/WX4i1Jq_o0Y",
+    email: "1234@gmai;.com",
+    phone: "090-0909-0909",
+    company: {
+      name: "テスト株式会社"
+    },
+    website: "https://google.com"
+  };
+});
 
 export const Users = () => {
   return (
     <SContainer>
       <h2>ユーザー一覧</h2>
       <SearchInput />
+      <USerArea>
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </USerArea>
     </SContainer>
   );
 };
 const SContainer = styled.div`
   text-align: center;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+const USerArea = styled.div`
+  padding-top: 40px;
+  widows: 100%;
+  display; grid;
+  grid-template-columns: repeat(auto-fil, minmax(200px, 1fr));
+  grid-gap: 20px;
 `;
